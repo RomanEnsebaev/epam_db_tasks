e9596fcca9031d6507ec39f8d9d03a6686d56bf2
RomanEnsebaev <87444715+RomanEnsebaev@users.noreply.github.com>
2025-04-03 19:54:55 +0500

    Add files via upload

---
+++ task_3/DELETE_Yensebayev_Roman.sql
@@ -0,0 +1,19 @@
+delete 
+from film_actor 
+where film_id = 1002;
+
+delete 
+from inventory 
+where film_id = 1002;
+
+delete 
+from film 
+where film_id = 1002;
+
+
+delete from payment 
+where customer_id = 69;
+
+delete from rental 
+where customer_id = 69;
+
